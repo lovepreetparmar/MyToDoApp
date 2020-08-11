aeaf92bbe1c88a58db99b8af844f059995707917
lovepreetparmar <lplovepreetparmar@gmail.com>
2020-08-11 22:52:29 +0530

    Updates

---
+++ precache-manifest.6b32f2dbbb33ddb75e93a8111bde9d1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05107a475933e51cc9a5aab19bda81e9",
+    "url": "/MyToDoApp/index.html"
+  },
+  {
+    "revision": "89f0016d3f7f90aa9cb9",
+    "url": "/MyToDoApp/static/css/2.06c478a6.chunk.css"
+  },
+  {
+    "revision": "89f0016d3f7f90aa9cb9",
+    "url": "/MyToDoApp/static/js/2.8fefcebc.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/MyToDoApp/static/js/2.8fefcebc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9196c187a1745864b958",
+    "url": "/MyToDoApp/static/js/main.e7b2fe97.chunk.js"
+  },
+  {
+    "revision": "bc2d1c4b2bf0de6d420a",
+    "url": "/MyToDoApp/static/js/runtime-main.5ef0f1f6.js"
+  }
+]);
